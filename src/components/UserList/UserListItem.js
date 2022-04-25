14026f47569f8aca3a8b7351eb08cac3552d30ac
jeffrey125 <jeffreyasilo8@gmail.com>
2022-04-25 15:26:42 +0800

    Fixed UserListItem Border

--- src/components/UserList/UserListItem.js
+++ src/components/UserList/UserListItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const UserListItem = props => {
   return (
-    <li className="text-2xl font-bold border-gray-200-200 border-2 p-4 shadow shadow-gray-300 animate-fadeIn">
+    <li className="text-2xl font-bold border-gray-300 border-2 p-4 shadow shadow-gray-300 animate-fadeIn">
       {props.user} {props.age}
     </li>
   );
